edbb2febd035e0738b783eaf607a0b37442789c1
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-24 20:07:43 +0300

    Add: TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+
+import TodoList from "./Todo/TodoList";
+
 function App() {
-  return (
-    <div className='wrapper'></div>
-  );
+    return (
+        <div className='wrapper'>
+            <h1>React Tutorial</h1>
+            <TodoList/>
+        </div>
+    );
 }
 
 export default App;
